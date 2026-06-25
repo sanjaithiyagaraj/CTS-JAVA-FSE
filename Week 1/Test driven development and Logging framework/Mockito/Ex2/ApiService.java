f8d06800da3e2d48b0cfd43d2956a8b4e959808d
sanjaithiyagaraj <92373720+sanjaithiyagaraj@users.noreply.github.com>
2026-06-25 13:31:44 +0530

    Create ApiService.java

---
+++ Week 1/Test driven development and Logging framework/Mockito/Ex2/ApiService.java
@@ -0,0 +1,12 @@
+public class ApiService {
+
+    private RestClient restClient;
+
+    public ApiService(RestClient restClient) {
+        this.restClient = restClient;
+    }
+
+    public String fetchData() {
+        return "Fetched " + restClient.getResponse();
+    }
+}
